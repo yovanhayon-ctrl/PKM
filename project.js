2c96c4fbcdbd6d2017f5d30c2d6fbd0d830aa210
yovanhayon-ctrl <yovanhayon@gmail.com>
2025-11-09 19:58:17 +0700

    Delete project.js

--- project.js
+++
@@ -1,5 +0,0 @@
-// Menampilkan tahun otomatis di footer
-document.addEventListener("DOMContentLoaded", function () {
-  const yearSpan = document.getElementById("year");
-  if (yearSpan) yearSpan.textContent = new Date().getFullYear();
-});
